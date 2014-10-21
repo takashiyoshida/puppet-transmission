542d7330141d8941b503fdd783e6b87c15dfb3f4
Takashi Yoshida <takashi@takashiyoshida.org>
2014-10-21 22:03:40 +0800

    Fix package name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class transmission {
-  package { 'transmission':
+  package { 'Transmission':
     ensure     => 'installed',
     provider   => 'appdmg',
     source     => 'http://download.transmissionbt.com/files/Transmission-2.82.dmg'
