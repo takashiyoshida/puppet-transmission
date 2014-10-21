9b14cc0c6dff550714624804a90b7515f6ae5830
Takashi Yoshida <takashi@takashiyoshida.org>
2014-10-21 21:54:56 +0800

    Fix Travis CI

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class transmission {
   package { 'transmission':
+    ensure     => 'installed',
     provider   => 'appdmg',
-    source => 'http://download.transmissionbt.com/files/Transmission-2.82.dmg'
+    source     => 'http://download.transmissionbt.com/files/Transmission-2.82.dmg'
   }
 }
